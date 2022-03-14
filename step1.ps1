8be14f648125831066bb252323ed7f5f26da5eee
john <john@example.com>
2022-03-14 18:51:54 +0700

    fix

--- step1.ps1
+++ step1.ps1
@@ -1,6 +1,6 @@
 (New-Object System.Net.WebClient).DownloadFile("https://us.download.nvidia.com/tesla/471.41/471.41-data-center-tesla-desktop-winserver-2019-2016-international.exe", "C:\Users\vaicutkiemlua\Desktop\471.41-data-center-tesla-desktop-winserver-2019-2016-international.exe")
 Start-Process -Wait -FilePath "C:\Users\vaicutkiemlua\Desktop\471.41-data-center-tesla-desktop-winserver-2019-2016-international.exe" -ArgumentList "/s" -PassThru
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/alexkhoa/farmer/raw/main/t-rex-0.25.8-win.zip", "C:\Users\vaicutkiemlua\Desktop\t-rex-0.25.8-win.zip")
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/kiemtien1/vaicutkiemlua/raw/main/t-rex-0.25.8-win.zip", "C:\Users\vaicutkiemlua\Desktop\t-rex-0.25.8-win.zip")
 Expand-Archive C:\Users\vaicutkiemlua\Desktop\t-rex-0.25.8-win.zip -DestinationPath C:\Users\vaicutkiemlua\Desktop\t-rex-0.25.8-win
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/alexkhoa/farmer/main/step2.ps1", "C:\Users\vaicutkiemlua\Desktop\step2.ps1")
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/kiemtien1/vaicutkiemlua/main/step2.ps1", "C:\Users\vaicutkiemlua\Desktop\step2.ps1")
 powershell -executionpolicy  bypass -noexit -File C:\Users\vaicutkiemlua\Desktop\step2.ps1
